4b5ce60ea7f927eea8781853ba9e4a9e8889a009
tiger <biguptiger@gmail.com>
2023-09-19 16:53:16 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('index.js loaded');
+// CSSセレクタを使ってDOMツリー中のh2要素を取得する
+const heading = document.querySelector("h2");
+// h2要素に含まれるテキストコンテンツを取得する
+const headingText = heading.textContent;
+
+// button要素を作成する
+const button = document.createElement("button");
+button.textContent = "Push Me";
+// body要素の子要素としてbuttonを挿入する
+document.body.appendChild(button);
